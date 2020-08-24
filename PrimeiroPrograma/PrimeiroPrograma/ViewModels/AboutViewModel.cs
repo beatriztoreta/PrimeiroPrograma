9e4ee382eea532ec7592b04cc02cf69dfbf62cd0
Beatriz Toreta Pereira <beatriztoreta@hotmail.com>
2020-08-24 11:17:15 -0300

    Teste
    Teste

---
+++ PrimeiroPrograma/PrimeiroPrograma/ViewModels/AboutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PrimeiroPrograma.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(ExecutarComando);
+            /*async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart")*/
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+        private void ExecutarComando(object obj)
+        {
+            var duracao = TimeSpan.FromSeconds(10);
+            Vibration.Vibrate(duracao);
+            Browser.OpenAsync("https://aka.ms/xamain-quickstart");
+        }
+    }
+}
---
+++ PrimeiroPrograma/PrimeiroPrograma/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using PrimeiroPrograma.ViewModels;
+
+namespace PrimeiroPrograma.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
